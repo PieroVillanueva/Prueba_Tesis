2e808bbe12fa927b774f20424e4f96511a29a828
PieroVillanueva <pierofabrizzio9@hotmail.com>
2022-08-22 16:37:58 -0500

    Sexto

---
+++ Assets/02.Resources/12. Riesgo Logistico/TimeLine/TL_Secuencias.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TL_Secuencias : MonoBehaviour
+{
+    PlayableDirector director;
+    public PlayableAsset[] cinematicas;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        director = GetComponent<PlayableDirector>();
+    }
+
+    
+
+    private void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.A))
+        {
+            reproducirCinematica(0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            reproducirCinematica(1);
+        }*/
+    }
+    public void reproducirCinematica(int i)
+    {
+        director.playableAsset = cinematicas[i];
+        director.Play();
+    }
+}
