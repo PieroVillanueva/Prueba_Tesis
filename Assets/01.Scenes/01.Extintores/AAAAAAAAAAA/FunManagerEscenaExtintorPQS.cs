f881dd980741f9de720aa7eb4d2d168a16ca1f16
PieroVillanueva <pierofabrizzio9@hotmail.com>
2022-08-27 11:35:19 -0500

    v0.1.0

---
+++ Assets/01.Scenes/01.Extintores/AAAAAAAAAAA/FunManagerEscenaExtintorPQS.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunManagerEscenaExtintorPQS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] titulos;
+    public GameObject[] contenido;
+    public BoxCollider[] colliders;
+    void Start()
+    {
+        //solicitarExtintores();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void solicitarExtintores() => StartCoroutine(mostrarExtintores());
+
+    IEnumerator mostrarExtintores()
+    {
+        for (int i = 0; i < titulos.Length; i++)
+        {
+            yield return new WaitForSeconds(0.25f);
+            titulos[i].SetActive(true);
+            yield return new WaitForSeconds(0.18f);
+            contenido[i].SetActive(true);
+            yield return new WaitForSeconds(0.1f);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            colliders[i].enabled = true;
+        }
+        
+    }
+
+}
---
+++ Assets/01.Scenes/01.Extintores/AAAAAAAAAAA/FunManagerIntroExtintores.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunManagerIntroExtintores : MonoBehaviour
+{
+    [Header("Audios")]
+    public AudioSource reproductorAudios;
+    public AudioClip[] listaAudios;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(reproducirAudio(0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator reproducirAudio(int pos)
+    {
+        reproductorAudios.clip = listaAudios[pos];
+        if (pos==0)
+        {
+            yield return new WaitForSeconds(3.00f);
+        }        
+        reproductorAudios.Play();
+    }
+
+}
